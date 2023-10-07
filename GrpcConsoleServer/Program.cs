8892c60ca74af4f194bcb6de1e86a6bdc04b2d6f
Gon <krdn.net@gmail.com>
2023-10-07 14:04:03 +0900

    초기 커밋.

---
+++ GrpcConsoleServer/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using GrpcConsoleServer;
+using Helloworld;
+
+const int Port = 30051;
+Server server = new Server
+{
+    Services = { Greeter.BindService(new GreeterServer()) },
+    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+};
+server.Start();
+
+Console.WriteLine("Greeter server listening on port " + Port);
+Console.WriteLine("Press any key to stop the server...");
+Console.ReadKey();
+
+server.ShutdownAsync().Wait();
