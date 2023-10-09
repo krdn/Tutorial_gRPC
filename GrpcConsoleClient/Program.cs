777291e7ed7557c001049556c0d8289650ca0a29
Gon <krdn.net@gmail.com>
2023-10-09 10:52:00 +0900

    C# Grpc Client With Console 코드 추가

---
+++ GrpcConsoleClient/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+using Helloworld;
+
+Channel channel = new Channel("127.0.0.1:30051", ChannelCredentials.Insecure);
+
+var client = new Greeter.GreeterClient(channel);
+string user = "KRDN";
+
+var reply = client.SayHello(new HelloRequest { Name = user });
+Console.WriteLine("Greeting: " + reply.Message);
+
+channel.ShutdownAsync().Wait();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Tutorial_gRPC
+
+
+[grpc - console server](https://yogingang.tistory.com/227)
+
+
+[grpc - console client](https://yogingang.tistory.com/229)
+
+
