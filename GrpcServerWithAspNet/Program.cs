4330a406fc9ccc3dc4c5558f511dd4868711d14d
Gon <krdn.net@gmail.com>
2023-10-11 23:12:30 +0900

    C# Grpc Test With GrpcUrl

--- GrpcServerWithAspNet/Program.cs
+++ GrpcServerWithAspNet/Program.cs
@@ -4,8 +4,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
